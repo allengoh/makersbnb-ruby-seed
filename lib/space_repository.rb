260d94d3eda28eae8008598697f4a37c363c6a1a
alexandrashelley <alexbshelley@gmail.com>
2022-08-15 17:49:42 +0100

    create and all methods passing

---
+++ lib/space_repository.rb
@@ -0,0 +1,33 @@
+require_relative 'space'
+
+class SpaceRepository
+  def all
+    sql = 'SELECT * FROM spaces;'
+    result = DatabaseConnection.exec_params(sql, [])
+
+    spaces = []
+
+    result.each do |record|
+      space = Space.new
+      space.id = record['id'].to_i
+      space.name = record['name']
+      space.description = record['description']
+      space.price_per_night = record['price_per_night']
+      space.user_id = record['user_id'].to_i
+      spaces << space
+    end
+
+    return spaces
+  end
+
+  def create(space)
+    sql = 'INSERT INTO spaces (name, description, price_per_night, user_id) VALUES ($1, $2, $3, $4);'
+    params = [space.name, space.description, space.price_per_night, space.user_id]
+    result = DatabaseConnection.exec_params(sql, params)
+  end
+end
+
+
+
+
+
