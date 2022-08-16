49a94e226fb17f830de5b1e5bf3adfee72673d0d
Curtis Turk <curtis.c.turk@outlook.com>
2022-08-16 14:16:56 +0100

    Added the spaces page with the list of seed spaces

--- app.rb
+++ app.rb
@@ -1,8 +1,19 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
+require_relative 'lib/database_connection'
+require_relative 'lib/space_repository'
+
+DatabaseConnection.connect('makersbnb')
 
 class Application < Sinatra::Base
   configure :development do
     register Sinatra::Reloader
+  end
+
+  get '/spaces' do
+    repo = SpaceRepository.new
+
+    @spaces = repo.all
+    return erb(:spaces)
   end
 end
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -3,15 +3,15 @@ require "rack/test"
 require_relative '../../app'
 require 'json'
 
-def reset_albums_table
-  seed_sql = File.read('spec/seeds/albums_seeds.sql')
-  connection = PG.connect({ host: '127.0.0.1', dbname: 'music_library_test' })
+def reset_makersbnb_table
+  seed_sql = File.read('spec/seeds/seeds.sql')
+  connection = PG.connect({ host: '127.0.0.1', dbname: 'makersbnb_test' })
   connection.exec(seed_sql)
 end
 
 describe Application do
   before(:each) do 
-    reset_albums_table
+    reset_makersbnb_table
   end
   
   # This is so we can use rack-test helper methods.
@@ -20,4 +20,16 @@ describe Application do
   # We need to declare the `app` value by instantiating the Application
   # class so our tests work.
   let(:app) { Application.new }
+
+  context "GET /spaces" do
+    it "shows the list of spaces" do
+      response = get('/spaces')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to include("<div>Name: Luxurious Apartment with a Sea View</div>")
+    
+    end
+  end
+
+
 end
